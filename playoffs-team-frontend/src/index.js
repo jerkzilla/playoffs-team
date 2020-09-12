fda5b4278dad160de25b10c6fec5590e550c5b95
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-09-12 18:16:54 -0500

    put back fetch

--- playoffs-team-frontend/src/index.js
+++ playoffs-team-frontend/src/index.js
@@ -17,74 +17,74 @@ function getTeamsData() {
     })
 }
 
-// function renderTeam(teamObj) {
-//     let newDiv = document.createElement("div")
-//     let newP = document.createElement("p")
-//     let addBtn = document.createElement("button")
-//     let newUl = document.createElement("ul")
-//     newDiv.className = "card"
-//     newDiv.setAttribute("data-id", teamObj.id)
-//     newP.innerText = teamObj.values
-//     addBtn.innerText = "Add Player"
-//     addBtn.setAttribute("data-team-id", teamObj.id)
-//     addBtn.addEventListener("click", addPlayer)
+function renderTeam(teamObj) {
+    let newDiv = document.createElement("div")
+    let newP = document.createElement("p")
+    let addBtn = document.createElement("button")
+    let newUl = document.createElement("ul")
+    newDiv.className = "card"
+    newDiv.setAttribute("data-id", teamObj.id)
+    newP.innerText = teamObj.values
+    addBtn.innerText = "Add Player"
+    addBtn.setAttribute("data-team-id", teamObj.id)
+    addBtn.addEventListener("click", addPlayer)
 
 
-//     newDiv.appendChild(newP)
-//     newDiv.appendChild(addBtn)
-//     newDiv.appendChild(newUl)
-//     main.appendChild(newDiv)
+    newDiv.appendChild(newP)
+    newDiv.appendChild(addBtn)
+    newDiv.appendChild(newUl)
+    main.appendChild(newDiv)
 
-//     teamObj.players.forEach(player => renderPlayer(player));
-// }
+    teamObj.players.forEach(player => renderPlayer(player));
+}
 
-// function renderPlayer(player) {
-//     let teamId = player.team_id
-//     const div = document.querySelector(`div[data-id='${teamId}']`)
-//     const ul = div.getElementsByTagName("ul")[0]
-//     let li = document.createElement("li")
-//     let btn = document.createElement("button")
-//     btn.className = "release"
-//     btn.innerText = "Release"
-//     btn.setAttribute("data-player-id", player.id)
-//     btn.addEventListener("click", removePlayer)
-//     li.innerText = `${player.firstName} (${player.lastName})`
-//     li.appendChild(btn)
-//     ul.appendChild(li)
-// }
+function renderPlayer(player) {
+    let teamId = player.team_id
+    const div = document.querySelector(`div[data-id='${teamId}']`)
+    const ul = div.getElementsByTagName("ul")[0]
+    let li = document.createElement("li")
+    let btn = document.createElement("button")
+    btn.className = "release"
+    btn.innerText = "Release"
+    btn.setAttribute("data-player-id", player.id)
+    btn.addEventListener("click", removePlayer)
+    li.innerText = `${player.firstName} (${player.lastName})`
+    li.appendChild(btn)
+    ul.appendChild(li)
+}
 
 
 
-// function addPlayer(e) {
-//     e.preventDefault();
+function addPlayer(e) {
+    e.preventDefault();
 
-//     fetch(PLAYERS_URL, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Accept": "application/json"
-//         },
-//         body: JSON.stringify({ team_id: e.target.dataset.teamId })
-//     }).then(resp => {
-//         return resp.json()
-//     }).then(obj => {
-//         if (obj.message) {
-//             alert(obj.message)
-//         } else {
-//             renderPlayer(obj)
-//         }
-//     })
-// }
+    fetch(PLAYERS_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({ team_id: e.target.dataset.teamId })
+    }).then(resp => {
+        return resp.json()
+    }).then(obj => {
+        if (obj.message) {
+            alert(obj.message)
+        } else {
+            renderPlayer(obj)
+        }
+    })
+}
 
-// function removePlayer(e) {
-//     e.preventDefault();
-//     e.target.parentElement.remove()
-//     fetch(`http://localhost:3000/players/${ e.target.dataset.playerId }`, {
-//         method: "DELETE",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Accept": "application/json"
-//         }
-//     })
-// }
+function removePlayer(e) {
+    e.preventDefault();
+    e.target.parentElement.remove()
+    fetch(`http://localhost:3000/players/${ e.target.dataset.playerId }`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    })
+}
 
