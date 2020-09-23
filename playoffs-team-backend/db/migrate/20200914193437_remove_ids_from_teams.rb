cf17f9ac572d03741d33b00507485733ccfd9747
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-09-23 13:41:12 -0500

    reset db

--- playoffs-team-backend/db/migrate/20200914193437_remove_ids_from_teams.rb
+++
@@ -1,9 +0,0 @@
-class RemoveIdsFromTeams < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :teams, :first_player_id
-    remove_column :teams, :second_player_id
-    remove_column :teams, :third_player_id
-    remove_column :teams, :fourth_player_id
-    remove_column :teams, :fifth_player_id
-  end
-end
