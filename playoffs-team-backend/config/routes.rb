5905fd6789041f45e9083375f0a6556d37162c76
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-09-26 13:39:18 -0500

    init

--- playoffs-team-backend/config/routes.rb
+++ playoffs-team-backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :teams, only: [:index, :show, :create, :destroy]
   resources :players, only: [:index, :show]
-
+#
   get "players/players_by_ppg/:ppg_num", to: "players#players_by_ppg"
   get "players/players_by_position/:position", to: "players#players_by_position"
   get "players/players_by_apg/:apg_num", to: "players#players_by_apg"
